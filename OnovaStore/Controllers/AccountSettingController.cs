529163421ba4b0ce789793d16ac2167e2cb5a255
Nguyen Bao Vinh Loc <vinhloc1996@yahoo.com.vn>
2018-04-23 18:09:47 +0700

    Continue on user setting

---
+++ OnovaStore/Controllers/AccountSettingController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnovaStore.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class AccountSettingController : Controller
+    {
+
+        private readonly IClaimPrincipalManager _claimPrincipalManager;
+        private readonly IRestClient _restClient;
+
+        public AccountSettingController(IClaimPrincipalManager claimPrincipalManager, IRestClient restClient)
+        {
+            _claimPrincipalManager = claimPrincipalManager;
+            _restClient = restClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult UserInformation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UserInformation(object model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ChangePassword(object model)
+        {
+            return View();
+        }
+    }
+}
